06fdd8ac65ef699f4601a1d4a34c691d154ce035
Vetrichelvan2007 <vetrichelvan720@gmail.com>
2026-03-20 22:42:39 +0530

    Changed files (from git status): • StudentController.java • TeacherController.java • DataBaseConnection.java • Profile.html • ViewStudent.html • ViewSubject.html • TeacherQualificationDTO.java • ViewStudentDTO.java Suggested commit message:

---
+++ src/main/java/com/vetri/smartcampus/models/TeacherQualificationDTO.java
@@ -0,0 +1,70 @@
+package com.vetri.smartcampus.models;
+
+public class TeacherQualificationDTO {
+
+    private Long teacherId;
+    private String ugDegree;
+    private String pgDegree;
+    private String phdStatus;
+    private String specialization;
+    private String universityName;
+    private int yearOfPassing;
+
+    public TeacherQualificationDTO() {}
+
+    public Long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Long teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getUgDegree() {
+        return ugDegree;
+    }
+
+    public void setUgDegree(String ugDegree) {
+        this.ugDegree = ugDegree;
+    }
+
+    public String getPgDegree() {
+        return pgDegree;
+    }
+
+    public void setPgDegree(String pgDegree) {
+        this.pgDegree = pgDegree;
+    }
+
+    public String getPhdStatus() {
+        return phdStatus;
+    }
+
+    public void setPhdStatus(String phdStatus) {
+        this.phdStatus = phdStatus;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+
+    public int getYearOfPassing() {
+        return yearOfPassing;
+    }
+
+    public void setYearOfPassing(int yearOfPassing) {
+        this.yearOfPassing = yearOfPassing;
+    }
+}
---
+++ src/main/java/com/vetri/smartcampus/models/ViewStudentDTO.java
@@ -0,0 +1,52 @@
+package com.vetri.smartcampus.models;
+
+public class ViewStudentDTO {
+
+    private String studentName;
+    private String studentRegisterNumber;
+    private String department;
+    private int semester;
+    private String status;
+
+    public ViewStudentDTO() {}
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentRegisterNumber() {
+        return studentRegisterNumber;
+    }
+
+    public void setStudentRegisterNumber(String studentRegisterNumber) {
+        this.studentRegisterNumber = studentRegisterNumber;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
